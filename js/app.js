03c3f8d539b82f4d62a047a469ff836f589c2be3
Chris Garvin <chris.garvin@me.com>
2015-10-12 18:24:41 -0700

    added some added click effects that will be used later on

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 
 
+    $.getJSON("data/data.json", function(data){
 
-})
+      for(var i = 0; i < data.response.links.length; i++){
+
+        var className = data.response.links[i].name;
+        var linkAddress = data.response.links[i].url;
+
+        $('.'+ className).click(getUrl(className,linkAddress));
+
+      }
+
+    });
+
+    function getUrl(name,url){
+      return function(){
+        console.log(url);
+        $('.content').append("<a href='" + url +"'><span>"+ name + "</span></a>");
+      }
+    }
+
+
+});
